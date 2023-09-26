cadc17477d42e5c09e1bed058513d332e022f837
Bowie375 <2200013174@stu.pku.edu.cn>
2023-09-26 19:07:28 +0800

    float_negpwr2

--- bits.c
+++ bits.c
@@ -374,5 +374,17 @@ int float64_f2i(unsigned uf1, unsigned uf2) {
  *   Rating: 4
  */
 unsigned float_negpwr2(int x) {
+    int offset=0;
+
+    if(x>149){
+      return 0;
+    }else if(x>126){
+      offset=x-126;
+      return (0x800000)>>offset;
+    }else if(x>-128){
+      return (127-x)<<23;
+    }else{
+      return 0x7f800000;
+    }
     return 2;
 }
