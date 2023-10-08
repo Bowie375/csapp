2e1467f38adde12034a9bb0b367769c740eb1952
Bowie375 <2200013174@stu.pku.edu.cn>
2023-10-08 13:11:50 +0800

    add comments for fsm.cpp

--- fsm.cpp
+++ fsm.cpp
@@ -4,6 +4,7 @@
 #include <string>
 using namespace std;
 
+/*this table is the definition of the fsm*/
 map<pair<char,int>,char> transitTable={{{'0',0},'2'},{{'0',1},'1'},
                                     {{'1',0},'4'},{{'1',1},'5'},
                                     {{'2',0},'3'},{{'2',1},'6'},
@@ -12,21 +13,27 @@ map<pair<char,int>,char> transitTable={{{'0',0},'2'},{{'0',1},'1'},
                                     {{'5',0},'5'},{{'5',1},'0'},
                                     {{'6',0},'0'},{{'6',1},'2'}};
 
-string test="0123456";
+string init_points="0123456"; //the initial values we work on. They should give the same output for the fsm.
 
+/*this function is used to test if the 
+* last 'len' bits of the sequence 'seq'
+* satisfies our need.
+*/
 bool is_valid(int seq,int len){
     int tmp=0;
-    int strlen=test.length();
-    string arr=test;
+    int arrlen=init_points.length();
+    string arr=init_points;
 
+    /*apply fsm to the sequence and the init_points*/
     for(int i=len-1;i>=0;--i){
-        for(int j=0;j<strlen;++j){
+        for(int j=0;j<arrlen;++j){
             arr[j]=transitTable[{arr[j],(seq>>i)&1}];
         }
     }
     
+    /*check if all the outputs are the same*/
     tmp=arr[0];
-    for(int i=1;i<strlen;++i){
+    for(int i=1;i<arrlen;++i){
         if(arr[i]!=tmp){
             return false;
         }
@@ -36,11 +43,18 @@ bool is_valid(int seq,int len){
 }
 
 int main(){
-    bool flag=0;
-    for(int i=0;i<(1<<13);++i){
-        for(int j=1;j<=13;++j){
+    bool found=0;
+
+    /*we use the bit-sequence of 'i' to represent 
+    * our answer sequence and 'j' to represent the
+    * length of the sequence here.
+    */
+    for(int j=1;j=13;++j){
+        for(int i=0;i<(1<<j);++i){
             if(is_valid(i,j)){
-                flag=1;
+                found=1;
+
+                /*output the sequence according to bit order*/
                 for(int k=j-1;k>=0;--k){
                     cout<<((i>>k)&1);
                 }
@@ -48,7 +62,8 @@ int main(){
             }
         }
     }
-    if(!flag){
+
+    if(!found){
         cout<<"no answer!"<<endl;
     }
     return 0;
